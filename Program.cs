1aa6c66bcfc4cd1c05de7a8475a77a90f694f1d2
Oleksandr Khomenko <alex.khomenko96@gmail.com>
2019-11-10 19:42:17 -0500

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 2, 3, 1, 5, 4, 6, 4 };
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dict.ContainsKey(arr[i]))
+                    dict.Remove(arr[i]);
+                    dict.Add(arr[i], i);
+            }
+
+            var b = dict.Keys;
+            foreach (int a in b)
+            Console.Write(a + " ");
+            Console.ReadKey();
+        }
+    }
+}
